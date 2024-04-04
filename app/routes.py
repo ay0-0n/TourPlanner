5630d9d6bb7cdc2cebd2325e8379ba3c9ea7bc8e
ay0-0n <sartizayo.on@gmail.com>
2024-04-04 23:54:59 +0600

    modified signup, added password hashing, no work done on login. Added a dependecy file with instructions

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app, db
 from flask import redirect, render_template, url_for, request, session
-from app.models import User
+from app.models import *
+from flask_bcrypt import Bcrypt
 
 @app.route('/')
 def index():
@@ -30,7 +31,10 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/signup' , methods = ['GET', 'POST'])
+
+bcrypt = Bcrypt()
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         first_name = request.form['first_name']
@@ -39,21 +43,23 @@ def signup():
         email = request.form['email']
         username = request.form['username']
         password = request.form['password']
-        user_type = request.form['user_type']
+        role_id = request.form['role_id']  # Retrieve role_id (user type) from form
 
         existing_user = User.query.filter_by(username=username).first()
 
         if existing_user:
             return render_template('signup.html', error_message='Username already exists!')
 
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        # Create a new User object with role_id (user type)
         new_user = User(first_name=first_name, last_name=last_name, phone_number=phone_number,
-                        email=email, username=username, password=password, user_type=user_type)
-        
+                        email=email, username=username, password=hashed_password, role_id=role_id)
+
         db.session.add(new_user)
         db.session.commit()
 
         return redirect(url_for('login'))
-
 
     return render_template('signup.html')
 
