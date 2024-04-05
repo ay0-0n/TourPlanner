64352e87acbcaa1150c1a9ee3b010f52886cf9b9
ay0-0n <sartizayo.on@gmail.com>
2024-04-05 21:58:23 +0600

    implemented password recovery otp

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,15 @@ from app import app, db
 from flask import redirect, render_template, url_for, request, session, flash
 from app.models import *
 from flask_bcrypt import Bcrypt
+from flask_mail import Mail, Message
+from random import *
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'tourplanner.dev@gmail.com'
+app.config['MAIL_PASSWORD'] = 'kmox qqhn pkzk xbws'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 @app.route('/')
 @app.route('/home')
@@ -51,10 +60,13 @@ def signup():
         password = request.form['password']
         role_id = request.form['role_id']
 
+        existing_email = User.query.filter_by(email=email).first()
         existing_user = User.query.filter_by(username=username).first()
 
         if existing_user:
             return render_template('signup.html', error_message='Username already exists!')
+        if existing_email:
+            return render_template('signup.html', error_message='Email already exists!')
 
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
@@ -68,6 +80,43 @@ def signup():
 
     return render_template('signup.html')
 
+mail = Mail(app)
+otp = randint(1000, 9999)
+
+def send_mail(email):
+    msg = Message('OTP: Tour planner', sender='tourplanner.dev@gmail.com', recipients=[email])
+    msg.body = str(otp)
+    mail.send(msg)
+    return redirect(url_for('otpcheck', email=email))
+
+@app.route('/otpcheck', methods=['GET','POST'])
+def otpcheck():
+    if request.method == 'POST':
+        user_otp = request.form['otp']
+        if otp == int(user_otp):
+            return redirect(url_for('resetpass'))
+        else:
+            return render_template('otpcheck.html', error_message='Invalid OTP, please try again!')
+    return render_template('otpcheck.html')
+
+@app.route('/resetpass', methods=['GET', 'POST'])
+def resetpass():
+    email = session.get('email')
+    if request.method == 'POST':
+        password = request.form['password']
+        confirm_password = request.form['confirm-password']
+        if password == confirm_password:
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            user = User.query.filter_by(email=email).first() 
+            user.password = hashed_password
+            db.session.commit()
+            session.pop('email')
+            return redirect(url_for('login'))
+        else:
+            return render_template('resetpass.html', error_message='Password does not match, please try again!')
+    
+    return render_template('resetpass.html')
+
 @app.route('/forgetpass', methods=['GET', 'POST'])
 def forgetpass():
     if request.method == 'POST':
@@ -75,10 +124,13 @@ def forgetpass():
         user = User.query.filter_by(email=email).first()
 
         if user:
-            return redirect(url_for('login'))
+            session['email'] = email
+            send_mail(email)
+            return redirect(url_for('otpcheck'))
         else:
             return render_template('forgetpass.html', error_message='Email not found, please try again!')
     return render_template('forgetpass.html')
+
 
 @app.route('/tourist')
 def tourist():
