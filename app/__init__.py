fde829939a1dac2a79a6b25e5bcd9fbf9e677f34
TURJO SAJID <165931980+turjosajid@users.noreply.github.com>
2024-04-27 18:04:05 +0600

    changed db name

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = '11235813'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tourplanner.db"
 
 db = SQLAlchemy(app)
 
